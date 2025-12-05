ae7ae09b93eeb97f1aabe95457d4972bafb43307
Ayoub Amer <ayouub.ameur@gmail.com>
2025-12-05 17:42:15 +0100

    feat : add support for .env

--- src/main/java/com/buoya/crud/CrudApplication.java
+++ src/main/java/com/buoya/crud/CrudApplication.java
@@ -3,10 +3,18 @@ package com.buoya.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().filename(".env.local").load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
